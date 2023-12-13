d37f97a878c906ef7d6548e9007c62acb81f1bd1
Merel Smit <138596987+LeremTims@users.noreply.github.com>
2023-12-13 10:45:43 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def run_commands(commands):
+    for command in commands:
+        subprocess.run(command, shell=True)
+
+mac_address_commands = [
+    "ifconfig wlan0 down",
+    "ifconfig wlan0 hw ether 00:11:22:33:44:55",
+    "ifconfig wlan0 up"
+]
+
+monitor_mode_commands = [
+    "ifconfig wlan0 down",
+    "airmon-ng check kill",
+    "iwconfig wlan0 mode monitor",
+    "ifconfig wlan0 up",
+]
+
+# Run the first set of commands
+run_commands(mac_address_commands)
+print("[+] Ran MAC Address Command")
+
+# Run the second set of commands
+run_commands(monitor_mode_commands)
+print("[+] Ran Monitor Mode Command")
